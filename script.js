6966ba18b31e6db1cff6dc76b45257f327eded52
ahmad786-code <ahmadrazadev2@gmail.com>
2024-07-22 13:43:57 +0500

    contact form

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+ 
+
 ScrollReveal({ reset: true,scale: 1.50 });
 ScrollReveal({ distance: '-600px' });
 ScrollReveal().reveal('.animated-card-3', { 
@@ -24,138 +26,110 @@ ScrollReveal().reveal('.animated-card-3', {
     easing: 'cubic-bezier(0.5, 0, 0, 1)',
     reset: false
   });
-  
+   
 
-  particlesJS("particles-js", {
-    "particles": {
-      "number": {
-        "value": 290,
-        "density": {
-          "enable": true,
-          "value_area": 800
-        }
-      },
-      "color": {
-        "value": "#ffffff"
-      },
-      "shape": {
-        "type": "triangle",
-        "stroke": {
-          "width": 0,
-          "color": "#000000"
-        },
-        "polygon": {
-          "nb_sides": 10
-        },
-        "image": {
-          "src": "img/github.svg",
-          "width": 100,
-          "height": 100
-        }
-      },
-      "opacity": {
-        "value": 0.5,
-        "random": false,
-        "anim": {
-          "enable": false,
-          "speed": 1,
-          "opacity_min": 0.1,
-          "sync": false
-        }
-      },
-      "size": {
-        "value": 3,
-        "random": true,
-        "anim": {
-          "enable": false,
-          "speed": 40,
-          "size_min": 0.1,
-          "sync": false
-        }
-      },
-      "line_linked": {
-        "enable": true,
-        "distance": 150,
-        "color": "#ffffff",
-        "opacity": 0.4,
-        "width": 1
-      },
-      "move": {
-        "enable": true,
-        "speed": 6,
-        "direction": "none",
-        "random": true,
-        "straight": false,
-        "out_mode": "out",
-        "bounce": true,
-        "attract": {
-          "enable": false,
-          "rotateX": 600,
-          "rotateY": 1200
-        }
-      }
-    },
-    "interactivity": {
-      "detect_on": "window",
-      "events": {
-        "onhover": {
-          "enable": true,
-          "mode": "repulse"
-        },
-        "onclick": {
-          "enable": true,
-          "mode": "push"
-        },
-        "resize": true
-      },
-      "modes": {
-        "grab": {
-          "distance": 140,
-          "line_linked": {
-            "opacity": 1
-          }
-        },
-        "bubble": {
-          "distance": 400,
-          "size": 100,
-          "duration": 2,
-          "opacity": 8,
-          "speed": 3
-        },
-        "repulse": {
-          "distance": 100,
-          "duration": 0.4
-        },
-        "push": {
-          "particles_nb": 4
-        },
-        "remove": {
-          "particles_nb": 2
-        }
-      }
-    },
-    "retina_detect": true
-  });
+  var WIDTH, HEIGHT, canvas, con, g;
+var pxs = [];
+var rint = 70;
+$(document).ready(function() {
+    var windowSize = function() {
+        WIDTH = $('.galaxy-wrapper').innerWidth();
+        HEIGHT = $('.galaxy-wrapper').innerHeight();
+        canvas = $('#galaxy');
+        canvas.attr('width', WIDTH).attr('height', HEIGHT);
+    };
   
+    windowSize();
   
-  $(document).ready(function () {
-    var stars = 800;
-    var $stars = $(".stars");
-    var r = 800;
-    for (var i = 0; i < stars; i++) {
-        var $star = $("<div/>").addClass("star");
-        $stars.append($star);
-    }
-    $(".star").each(function () {
-        var cur = $(this);
-        var s = 0.2 + (Math.random() * 2);
-        var curR = r + (Math.random() * 600);
-        cur.css({
-            transformOrigin: "0 0 " + curR + "px",
-            transform: " translate3d(0,0,-" + curR + "px) rotateY(" + (Math.random() * 360) + "deg) rotateX(" + (Math.random() * -50) + "deg) scale(" + s + "," + s + ")",
-        });
-    })
-    $(".star").hover(function () {
-        $(this).addClass("blink");
+    $(window).resize(function() {
+        windowSize();
     });
-})
+
+    con = canvas[0].getContext('2d');
+    for (var i = 0; i < 100; i++) {
+        pxs[i] = new Circle();
+        pxs[i].reset();
+    }
+    requestAnimationFrame(draw);
+});
+
+function draw() {
+    con.clearRect(0, 0, WIDTH, HEIGHT);
+    con.globalCompositeOperation = "lighter";
+    for (var i = 0; i < pxs.length; i++) {
+        pxs[i].fade();
+        pxs[i].move();
+        pxs[i].draw();
+    }
+    requestAnimationFrame(draw);
+}
+
+function Circle() {
+    this.s = {
+        ttl: 15000,
+        xmax: 5,
+        ymax: 2,
+        rmax: 17,
+        rt: 1,
+        xdef: 960,
+        ydef: 540,
+        xdrift: 2,
+        ydrift: 2,
+        random: true,
+        blink: true
+    };
+    this.reset = function() {
+        this.x = (this.s.random ? WIDTH * Math.random() : this.s.xdef);
+        this.y = (this.s.random ? HEIGHT * Math.random() : this.s.ydef);
+        this.r = ((this.s.rmax - 1) * Math.random()) + 1;
+        this.dx = (Math.random() * this.s.xmax) * (Math.random() < 0.5 ? -1 : 1);
+        this.dy = (Math.random() * this.s.ymax) * (Math.random() < 0.5 ? -1 : 1);
+        this.hl = (this.s.ttl / rint) * (this.r / this.s.rmax);
+        this.rt = Math.random() * this.hl;
+        this.s.rt = Math.random() + 1;
+        this.stop = Math.random() * 0.2 + 0.4;
+        this.s.xdrift *= Math.random() * (Math.random() < 0.5 ? -1 : 1);
+        this.s.ydrift *= Math.random() * (Math.random() < 0.5 ? -1 : 1);
+    };
+    this.fade = function() {
+        this.rt += this.s.rt;
+    };
+    this.draw = function() {
+        if (this.s.blink && (this.rt <= 0 || this.rt >= this.hl)) this.s.rt = this.s.rt * -1;
+        else if (this.rt >= this.hl) this.reset();
+        var newo = 1 - (this.rt / this.hl);
+        con.beginPath();
+        con.arc(this.x, this.y, this.r, 0, Math.PI * 2, true);
+        con.closePath();
+        var cr = this.r * newo;
+        g = con.createRadialGradient(this.x, this.y, 0, this.x, this.y, (cr <= 0 ? 1 : cr));
+        g.addColorStop(0.0, 'rgba(193,254,254,' + newo + ')');
+        g.addColorStop(this.stop, 'rgba(193,254,254,' + (newo * 0.2) + ')');
+        g.addColorStop(1.0, 'rgba(193,254,254,0)');
+        con.fillStyle = g;
+        con.fill();
+    };
+    this.move = function() {
+        this.x += (this.rt / this.hl) * this.dx;
+        this.y += (this.rt / this.hl) * this.dy;
+        if (this.x > WIDTH || this.x < 0) this.dx *= -1;
+        if (this.y > HEIGHT || this.y < 0) this.dy *= -1;
+    };
+    this.getX = function() {
+        return this.x;
+    };
+    this.getY = function() {
+        return this.y;
+    };
+};
+
+
+
+document.querySelector('.menu-icon').addEventListener('click', function() {
+  const nav = document.querySelector('.nav');
+  if (nav.style.left === '0px') {
+      nav.style.left = '-100%';
+  } else {
+      nav.style.left = '0px';
+  }
+});
